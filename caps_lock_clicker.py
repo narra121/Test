ae138e5d830353cfbeb4c106f30985d7d6024c85
narpra1 <narpra1@FIVE9.com>
2026-02-27 12:58:19 +0530

    Initial commit

---
+++ caps_lock_clicker.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+print("Starting Caps Lock double-press every minute. Press Ctrl+C to stop.")
+
+try:
+    while True:
+        pyautogui.press('capslock')
+        time.sleep(0.1)
+        pyautogui.press('capslock')
+        print(f"Caps Lock pressed twice at {time.strftime('%H:%M:%S')}")
+        time.sleep(60)
+except KeyboardInterrupt:
+    print("Stopped.")
